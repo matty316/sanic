bda697e2ec82f66dd96f31d41500006f73578b09
matty <mattreed1890@gmail.com>
2025-01-02 08:02:04 -0600

    add title screen

--- src/main.c
+++ src/main.c
@@ -3,11 +3,16 @@
 #include "environment.h"
 #include "common.h"
 
+typedef enum GameState {
+	Title, Gameplay
+} GameState;
+
 int main ()
 {
 	Player player;
 	LevelEnv env;
 	Camera2D camera;
+	GameState state = Title;
 
 	InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Hello Raylib");
 	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
@@ -21,19 +26,42 @@ int main ()
 	// game loop
 	while (!WindowShouldClose())
 	{
-		float deltaTime = GetFrameTime();
-		updatePlayer(&player, deltaTime, &env);
-		updateCamera(&camera, &player, &env);
-		
-		BeginDrawing();
+		switch (state)
+		{
+		case Title: {
+			if (IsKeyPressed(KEY_ENTER) || IsGestureDetected(GESTURE_TAP))
+			{
+				state = Gameplay;
+			}
+			BeginDrawing();
+			// TODO: Draw TITLE screen here!
+			DrawRectangle(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, SKYBLUE);
+			DrawText("SANIC", 20, 20, 40, DARKBLUE);
+			const char text[] = "PRESS ENTER";
+			int fontSize = 40;
+			int textWidth = MeasureText(text, fontSize);
+			DrawText(text, (SCREEN_WIDTH - textWidth) / 2.0f, SCREEN_HEIGHT / 2.0f, fontSize, DARKBLUE);
+			EndDrawing();
+			break;
+		}
+		case Gameplay: {
+			float deltaTime = GetFrameTime();
+			updatePlayer(&player, deltaTime, &env);
+			updateCamera(&camera, &player, &env);
 
-        ClearBackground(SKYBLUE);
-		BeginMode2D(camera);
-		drawEnv(&env);
-		drawPlayer(&player);
-		EndMode2D();
+			BeginDrawing();
+
+			ClearBackground(SKYBLUE);
+			BeginMode2D(camera);
+			drawEnv(&env);
+			drawPlayer(&player);
+			EndMode2D();
+
+			EndDrawing();
+			break;
+		}
+		}
 		
-		EndDrawing();
 	}
 
 	CloseWindow();
